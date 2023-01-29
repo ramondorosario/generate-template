4caf6fe2b7acf2c0827cfe5c1fb2af4a3408eb68
Ramon do Rosário <ramon.rosario@cubos.io>
2023-01-29 22:50:34 +0000

    style: page

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -41,62 +41,64 @@ export function NewCard({
   } para:`;
 
   return (
-    <div className={s.container} id={id}>
-      <header>
-        <div className={s.infos}>
-          <img
-            src={logoValue === "generic" ? GenericLogoIcon : FiebLogoIcon}
-            alt=""
-          />
-          <div style={{ gap: spaces.header, fontSize: fontSizes.header }}>
-            <h2>{amountJobsText}</h2>
-            <p>{data.role}</p>
+    <main>
+      <div className={s.container} id={id}>
+        <header>
+          <div className={s.infos}>
+            <img
+              src={logoValue === "generic" ? GenericLogoIcon : FiebLogoIcon}
+              alt=""
+            />
+            <div style={{ gap: spaces.header, fontSize: fontSizes.header }}>
+              <h2>{amountJobsText}</h2>
+              <p>{data.role}</p>
+              <p>
+                Código da vaga:{" "}
+                <span className={data.rp.amount >= 3 ? s.breakline : ""}>
+                  {data.rp.codes}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className={s.footerInfos}>
             <p>
-              Código da vaga:{" "}
-              <span className={data.rp.amount >= 3 ? s.breakline : ""}>
-                {data.rp.codes}
+              <img src={LocaleIcon} alt="" />
+              <span style={{ paddingTop: adjustPhone11 ? 15 : 0 }}>
+                {data.locale}
+              </span>
+            </p>
+            <p>
+              <img src={CalendarIcon} alt="" />
+              <span>
+                Período de inscrição: <br /> {data.registrationDeadline}
               </span>
             </p>
           </div>
-        </div>
+        </header>
 
-        <div className={s.footerInfos}>
-          <p>
-            <img src={LocaleIcon} alt="" />
-            <span style={{ paddingTop: adjustPhone11 ? 15 : 0 }}>
-              {data.locale}
-            </span>
-          </p>
-          <p>
-            <img src={CalendarIcon} alt="" />
-            <span>
-              Período de inscrição: <br /> {data.registrationDeadline}
-            </span>
-          </p>
-        </div>
-      </header>
+        <section>
+          <ul style={{ fontSize: fontSizes.list, gap: spaces.list }}>
+            {data.list.map((item, i) => (
+              <li key={item + i} dangerouslySetInnerHTML={{ __html: item }} />
+            ))}
+          </ul>
+        </section>
 
-      <section>
-        <ul style={{ fontSize: fontSizes.list, gap: spaces.list }}>
-          {data.list.map((item, i) => (
-            <li key={item + i} dangerouslySetInnerHTML={{ __html: item }} />
-          ))}
-        </ul>
-      </section>
-
-      <footer>
-        <div>
-          <img src={ClickIcon} alt="" />
+        <footer>
           <div>
-            <p>
-              Se interessou pela vaga? <br />
-              Cadastre seu currículo através do portal: <br />
-              www.trinnusrh.com.br
-            </p>
+            <img src={ClickIcon} alt="" />
+            <div>
+              <p>
+                Se interessou pela vaga? <br />
+                Cadastre seu currículo através do portal: <br />
+                www.trinnusrh.com.br
+              </p>
+            </div>
           </div>
-        </div>
-        <img src={LogoTrinnus} alt="" />
-      </footer>
-    </div>
+          <img src={LogoTrinnus} alt="" />
+        </footer>
+      </div>
+    </main>
   );
 }
--- src/page/domino/index.tsx
+++ src/page/domino/index.tsx
@@ -18,101 +18,105 @@ export function Domino() {
   }
 
   return (
-    <div className={s.container}>
-      <h2>Pontuação Dominó</h2>
+    <main>
+      <div className={s.container}>
+        <h2>Pontuação Dominó</h2>
 
-      {!ready && (
-        <>
-          <input
-            type="text"
-            placeholder="Nome do participante"
-            value={gamer}
-            onChange={(e) => setGamer(e.target.value)}
-          />
-
-          <ul>
-            {participants.map((p, i) => (
-              <li key={p.name + i}>{p.name}</li>
-            ))}
-          </ul>
-        </>
-      )}
-
-      {!!participants.length && ready && (
-        <div className={s.main}>
-          {participants.map((p) => (
-            <div
-              className={`${s.participants} ${
-                selectedGamer === p.name ? s.selected : ""
-              }`}
-              onClick={() => setSelectedGamer(p.name)}
-              key={p.name}
-            >
-              <p>{p.name}</p>
-              <p className={s.score}>{p.score}</p>
-            </div>
-          ))}
-        </div>
-      )}
-
-      {ready && (
-        <div className={s.buttons}>
-          {btns.map((b) => (
-            <button
-              className={button === b ? s.selected : ""}
-              key={b}
-              onClick={() => setButton(b)}
-            >
-              {b}
-            </button>
-          ))}
-        </div>
-      )}
-
-      <footer>
         {!ready && (
-          <button
-            style={{ backgroundColor: "green" }}
-            onClick={() => setReady(true)}
-          >
-            Iniciar
-          </button>
+          <>
+            <input
+              type="text"
+              placeholder="Nome do participante"
+              value={gamer}
+              onChange={(e) => setGamer(e.target.value)}
+            />
+
+            <ul>
+              {participants.map((p, i) => (
+                <li key={p.name + i}>{p.name}</li>
+              ))}
+            </ul>
+          </>
         )}
+
+        {!!participants.length && ready && (
+          <div className={s.main}>
+            {participants.map((p) => (
+              <div
+                className={`${s.participants} ${
+                  selectedGamer === p.name ? s.selected : ""
+                }`}
+                onClick={() => setSelectedGamer(p.name)}
+                key={p.name}
+              >
+                <p>{p.name}</p>
+                <p className={s.score}>{p.score}</p>
+              </div>
+            ))}
+          </div>
+        )}
+
         {ready && (
+          <div className={s.buttons}>
+            {btns.map((b) => (
+              <button
+                className={button === b ? s.selected : ""}
+                key={b}
+                onClick={() => setButton(b)}
+              >
+                {b}
+              </button>
+            ))}
+          </div>
+        )}
+
+        <footer>
+          {!ready && (
+            <button
+              style={{ backgroundColor: "green" }}
+              onClick={() => setReady(true)}
+            >
+              Iniciar
+            </button>
+          )}
+          {ready && (
+            <button
+              onClick={() => {
+                if (ready && button) {
+                  const find = participants.find(
+                    (p) => p.name === selectedGamer
+                  )!;
+                  find.score = find.score - button;
+                  setButton(null);
+                  setSelectedGamer("");
+
+                  return setParticipants([...participants]);
+                }
+              }}
+            >
+              Remover
+            </button>
+          )}
           <button
             onClick={() => {
               if (ready && button) {
                 const find = participants.find(
                   (p) => p.name === selectedGamer
                 )!;
-                find.score = find.score - button;
+                find.score = find.score + button;
                 setButton(null);
                 setSelectedGamer("");
 
                 return setParticipants([...participants]);
               }
+
+              !ready && addParticipant();
             }}
           >
-            Remover
+            Adicionar
           </button>
-        )}
-        <button
-          onClick={() => {
-            if (ready && button) {
-              const find = participants.find((p) => p.name === selectedGamer)!;
-              find.score = find.score + button;
-              setButton(null);
-              setSelectedGamer("");
-
-              return setParticipants([...participants]);
-            }
-
-            !ready && addParticipant();
-          }}
-        >
-          Adicionar
-        </button>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </main>
   );
 }
